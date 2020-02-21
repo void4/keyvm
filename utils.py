db66fab74cf18b5d4fed290c8fb6f600925903c4
void4 <arndtschnabel@freenet.de>
2020-02-21 20:00:31 +0100

    Add SBF utils

---
+++ utils.py
@@ -0,0 +1,28 @@
+def num(a):
+    c = ""
+    for i in bin(a)[2:]:
+        if i == "1":
+            c += "+"
+        c += "²"
+    return c
+
+def clear():
+    return "½"*8
+
+def pair(a,b):
+    c = ""
+    for i in bin(a)[2:].zfill(4):#can omit zfill here, but only if memory cell zero beforehand
+        if i == "1":
+            c += "+"
+        c += "²"
+
+    for i in bin(b)[2:].zfill(4):
+        if i == "1":
+            c += "+"
+        c += "²"
+
+    return c
+
+print(num(255))
+print(pair(7,7))
+print(clear())
